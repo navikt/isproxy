68c26913b0d040856622960430fe7a6783db3939
geir-waagboe <geir.arne.waagbo@nav.no>
2021-09-07 14:06:36 +0200

    Call dokdist

--- src/main/kotlin/no/nav/syfo/client/dokdist/DokdistClient.kt
+++ src/main/kotlin/no/nav/syfo/client/dokdist/DokdistClient.kt
@@ -1,13 +1,16 @@
 package no.nav.syfo.client.dokdist
 
 import io.ktor.client.features.*
+import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import net.logstash.logback.argument.StructuredArguments
+import no.nav.syfo.client.httpClientDefault
 import no.nav.syfo.client.sts.StsClient
 import no.nav.syfo.metric.COUNT_CALL_DOKDIST_DISTRIBUER_JOURNALPOST_FAIL
 import no.nav.syfo.metric.COUNT_CALL_DOKDIST_DISTRIBUER_JOURNALPOST_SUCCESS
+import no.nav.syfo.util.bearerHeader
 import org.slf4j.LoggerFactory
-import java.util.*
 
 class DokdistClient(
     dokdistBaseUrl: String,
@@ -15,36 +18,42 @@ class DokdistClient(
 ) {
 
     private val distribuerJournalpostUrl: String = "$dokdistBaseUrl$DISTRIBUER_JOURNALPOST_PATH"
+    private val httpClient = httpClientDefault()
 
     suspend fun distribuerJournalpost(
         dokdistRequest: DokdistRequest,
         callId: String
     ): DokdistResponse? {
-        return try {
-            val token = stsClient.token()
-            // TODO: Call dokdist api with sts-token ?
+        try {
+            val stsToken = stsClient.token()
+            val response: DokdistResponse = httpClient.post(distribuerJournalpostUrl) {
+                header(HttpHeaders.Authorization, bearerHeader(stsToken))
+                accept(ContentType.Application.Json)
+                contentType(ContentType.Application.Json)
+                body = dokdistRequest
+            }
             COUNT_CALL_DOKDIST_DISTRIBUER_JOURNALPOST_SUCCESS.increment()
-            DokdistResponse(
-                bestillingsId = UUID.randomUUID().toString()
-            )
+            return response
         } catch (e: ClientRequestException) {
-            handleUnexpectedResponseException(e.response, e.message)
+            handleUnexpectedResponseException(e.response, e.message, callId)
         } catch (e: ServerResponseException) {
-            handleUnexpectedResponseException(e.response, e.message)
+            handleUnexpectedResponseException(e.response, e.message, callId)
         }
+        return null
     }
 
     private fun handleUnexpectedResponseException(
         response: HttpResponse,
         message: String?,
-    ): DokdistResponse? {
+        callId: String,
+    ) {
         log.error(
-            "Error while requesting dokdist to distribuere journalpost with {}, {}",
+            "Error while requesting dokdist to distribuere journalpost with {}, {}, {}",
             StructuredArguments.keyValue("statusCode", response.status.value.toString()),
             StructuredArguments.keyValue("message", message),
+            StructuredArguments.keyValue("callId", callId),
         )
         COUNT_CALL_DOKDIST_DISTRIBUER_JOURNALPOST_FAIL.increment()
-        return null
     }
 
     companion object {
