5d8cdf7c03da373d662337da07c75584bf9f95c8
Anders Rognstad <anders.rognstad@nav.no>
2021-09-07 09:46:11 +0200

    first draft proxy with dokdist client

--- src/main/kotlin/no/nav/syfo/api/ProxyApi.kt
+++ src/main/kotlin/no/nav/syfo/api/ProxyApi.kt
@@ -2,25 +2,37 @@ package no.nav.syfo.api
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import no.nav.syfo.client.dokdist.DokdistClient
+import no.nav.syfo.client.dokdist.DokdistRequest
+import no.nav.syfo.util.callIdArgument
+import no.nav.syfo.util.getCallId
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 private val log: Logger = LoggerFactory.getLogger("no.nav.syfo")
 
-const val proxyPath = "/api/v1/send"
-const val testPath = "/test"
+const val proxyPath = "/api/v1"
+const val distribuerJournalpostPath = "/distribuerJournalpost"
 
-fun Route.registerProxyApi() {
+fun Route.registerProxyApi(
+    dokdistClient: DokdistClient
+) {
     route(proxyPath) {
-        get(testPath) {
+        post(distribuerJournalpostPath) {
+            val callId = getCallId()
             try {
-                call.respond(HttpStatusCode.OK, "Vellykket!")
-            } catch (e: IllegalArgumentException) {
-                val illegalArgumentMessage = "Could not get from proxyApi"
-                log.warn("$illegalArgumentMessage: {}", e.message)
-                call.respond(HttpStatusCode.BadRequest, e.message ?: illegalArgumentMessage)
+                val dokdistRequest = call.receive<DokdistRequest>()
+                val dokdistResponse =
+                    dokdistClient.distribuerJournalpost(dokdistRequest, callId)
+                        ?: throw RuntimeException("Failed to distribute journalpost: missing response")
+                call.respond(dokdistResponse)
+            } catch (e: Exception) {
+                val message = "Could not distribute journalpost"
+                log.error("$message: {}, {}", e.message, callIdArgument(callId))
+                call.respond(HttpStatusCode.BadRequest, e.message ?: message)
             }
         }
     }
--- src/main/kotlin/no/nav/syfo/metric/Metrics.kt
+++ src/main/kotlin/no/nav/syfo/metric/Metrics.kt
@@ -1,8 +1,25 @@
 package no.nav.syfo.metric
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Counter.builder
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 
 const val METRICS_NS = "isproxy"
 
 val METRICS_REGISTRY = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+const val CALL_DOKDIST_DISTRIBUER_JOURNALPOST_BASE = "${METRICS_NS}_call_dokdist_distribuer_journalpost"
+const val CALL_DOKDIST_DISTRIBUER_JOURNALPOST_SUCCESS = "${CALL_DOKDIST_DISTRIBUER_JOURNALPOST_BASE}_success_count"
+const val CALL_DOKDIST_DISTRIBUER_JOURNALPOST_FAIL = "${CALL_DOKDIST_DISTRIBUER_JOURNALPOST_BASE}_fail_count"
+
+val COUNT_CALL_DOKDIST_DISTRIBUER_JOURNALPOST_SUCCESS: Counter = builder(
+    CALL_DOKDIST_DISTRIBUER_JOURNALPOST_SUCCESS
+)
+    .description("Counts the number of successful calls to Dokdist - Distribuer Journalpost")
+    .register(METRICS_REGISTRY)
+val COUNT_CALL_DOKDIST_DISTRIBUER_JOURNALPOST_FAIL: Counter = builder(
+    CALL_DOKDIST_DISTRIBUER_JOURNALPOST_FAIL
+)
+    .description("Counts the number of failed calls to Dokdist - Distribuer Journalpost")
+    .register(METRICS_REGISTRY)
