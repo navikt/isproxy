57082314f72c6fef5dbd17138bacb90db54cd4c1
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-10-12 14:42:36 +0200

    Add proxies to get Oppfolgingstilfelle for person with/without Virksomhet

--- src/main/kotlin/no/nav/syfo/syfosyketilfelle/client/SyfosyketilfelleClient.kt
+++ src/main/kotlin/no/nav/syfo/syfosyketilfelle/client/SyfosyketilfelleClient.kt
@@ -8,8 +8,9 @@ import io.ktor.http.*
 import no.nav.syfo.client.httpClientDefault
 import no.nav.syfo.client.sts.StsClient
 import no.nav.syfo.domain.AktorId
-import no.nav.syfo.syfosyketilfelle.COUNT_CALL_SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_FAIL
-import no.nav.syfo.syfosyketilfelle.COUNT_CALL_SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_SUCCESS
+import no.nav.syfo.domain.Virksomhetsnummer
+import no.nav.syfo.syfosyketilfelle.*
+import no.nav.syfo.syfosyketilfelle.domain.KOppfolgingstilfelle
 import no.nav.syfo.syfosyketilfelle.domain.KOppfolgingstilfellePerson
 import no.nav.syfo.util.bearerHeader
 
@@ -44,7 +45,53 @@ class SyfosyketilfelleClient(
         }
     }
 
+    suspend fun oppfolgingstilfellePersonUtenArbeidsgiver(
+        aktorId: AktorId,
+    ): KOppfolgingstilfellePerson? {
+        val stsToken = stsClient.token()
+        try {
+            val url = "$syfosyketilfelleOppfolgingstilfellePersonUrl/${aktorId.value}$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_UTEN_ARBEIDSGIVER_PATH"
+            val response: HttpResponse = httpClient.get(url) {
+                header(HttpHeaders.Authorization, bearerHeader(stsToken))
+                accept(ContentType.Application.Json)
+            }
+            if (response.status == HttpStatusCode.NoContent) {
+                return null
+            }
+            val responseBody = response.receive<KOppfolgingstilfellePerson>()
+            COUNT_CALL_SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_UTEN_ARBEIDSGIVER_SUCCESS.increment()
+            return responseBody
+        } catch (responseException: ResponseException) {
+            COUNT_CALL_SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_UTEN_ARBEIDSGIVER_FAIL.increment()
+            throw responseException
+        }
+    }
+
+    suspend fun oppfolgingstilfellePersonVirksomhet(
+        aktorId: AktorId,
+        virksomhetsnummer: Virksomhetsnummer,
+    ): KOppfolgingstilfelle? {
+        val stsToken = stsClient.token()
+        try {
+            val url = "$syfosyketilfelleOppfolgingstilfellePersonUrl/${aktorId.value}/${virksomhetsnummer.value}"
+            val response: HttpResponse = httpClient.get(url) {
+                header(HttpHeaders.Authorization, bearerHeader(stsToken))
+                accept(ContentType.Application.Json)
+            }
+            if (response.status == HttpStatusCode.NoContent) {
+                return null
+            }
+            val responseBody = response.receive<KOppfolgingstilfelle>()
+            COUNT_CALL_SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_VIRKSOMHET_SUCCESS.increment()
+            return responseBody
+        } catch (responseException: ResponseException) {
+            COUNT_CALL_SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_VIRKSOMHET_FAIL.increment()
+            throw responseException
+        }
+    }
+
     companion object {
         const val SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH = "/kafka/oppfolgingstilfelle/beregn"
+        const val SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_UTEN_ARBEIDSGIVER_PATH = "/utenarbeidsgiver"
     }
 }
--- src/test/kotlin/no/nav/syfo/testhelper/mock/SyfosyketilfelleMock.kt
+++ src/test/kotlin/no/nav/syfo/testhelper/mock/SyfosyketilfelleMock.kt
@@ -7,12 +7,13 @@ import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import no.nav.syfo.application.api.authentication.installContentNegotiation
-import no.nav.syfo.syfosyketilfelle.client.SyfosyketilfelleClient
-import no.nav.syfo.syfosyketilfelle.domain.KOppfolgingstilfellePerson
-import no.nav.syfo.syfosyketilfelle.domain.KSyketilfelledag
-import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID
+import no.nav.syfo.syfosyketilfelle.client.SyfosyketilfelleClient.Companion.SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH
+import no.nav.syfo.syfosyketilfelle.client.SyfosyketilfelleClient.Companion.SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_UTEN_ARBEIDSGIVER_PATH
+import no.nav.syfo.syfosyketilfelle.domain.*
 import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_AKTORID
+import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID
 import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR
+import no.nav.syfo.testhelper.UserConstants.VIRKSOMHETSNUMMER_DEFAULT
 import no.nav.syfo.testhelper.getRandomPort
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -39,11 +40,35 @@ fun generateOppfolgingstilfellePerson() =
         utsendelsestidspunkt = LocalDateTime.now(),
     )
 
+fun generateOppfolgingstilfelle() =
+    KOppfolgingstilfelle(
+        aktorId = ARBEIDSTAKER_AKTORID.value,
+        orgnummer = VIRKSOMHETSNUMMER_DEFAULT.value,
+        tidslinje = listOf(
+            KSyketilfelledag(
+                LocalDate.now().minusDays(10),
+                null,
+            ),
+            KSyketilfelledag(
+                LocalDate.now().plusDays(10),
+                null,
+            ),
+        ),
+        sisteDagIArbeidsgiverperiode = KSyketilfelledag(
+            LocalDate.now().minusDays(1),
+            null,
+        ),
+        antallBrukteDager = 0,
+        oppbruktArbeidsgvierperiode = false,
+        utsendelsestidspunkt = LocalDateTime.now(),
+    )
+
 class SyfosyketilfelleMock {
     private val port = getRandomPort()
     val url = "http://localhost:$port"
 
     val oppfolgingstilfellePersonResponse = generateOppfolgingstilfellePerson()
+    val oppfolgingstilfelleResponse = generateOppfolgingstilfelle()
 
     val name = "syfosyketilfelle"
     val server = mockServer()
@@ -55,13 +80,33 @@ class SyfosyketilfelleMock {
         ) {
             installContentNegotiation()
             routing {
-                get("${SyfosyketilfelleClient.SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH}/${ARBEIDSTAKER_AKTORID.value}") {
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_AKTORID.value}") {
                     call.respond(oppfolgingstilfellePersonResponse)
                 }
-                get("${SyfosyketilfelleClient.SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH}/${ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID.value}") {
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID.value}") {
                     call.respond(HttpStatusCode.NoContent)
                 }
-                get("${SyfosyketilfelleClient.SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH}/${ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}") {
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}") {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_AKTORID.value}/$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_UTEN_ARBEIDSGIVER_PATH") {
+                    call.respond(oppfolgingstilfellePersonResponse)
+                }
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID.value}$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_UTEN_ARBEIDSGIVER_PATH") {
+                    call.respond(HttpStatusCode.NoContent)
+                }
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_UTEN_ARBEIDSGIVER_PATH") {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_AKTORID.value}/${VIRKSOMHETSNUMMER_DEFAULT.value}") {
+                    call.respond(oppfolgingstilfelleResponse)
+                }
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID.value}/${VIRKSOMHETSNUMMER_DEFAULT.value}") {
+                    call.respond(HttpStatusCode.NoContent)
+                }
+                get("$SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}/${VIRKSOMHETSNUMMER_DEFAULT.value}") {
                     call.respond(HttpStatusCode.BadRequest)
                 }
             }
