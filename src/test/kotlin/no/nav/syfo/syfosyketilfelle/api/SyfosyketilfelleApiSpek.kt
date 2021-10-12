f98370b7deed20f88ad6abbbf71c104ab7821d5d
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-10-12 16:42:09 +0200

    Add proxies to get Oppfolgingstilfelle for person with/without Virksomhet (#38)

--- src/test/kotlin/no/nav/syfo/syfosyketilfelle/api/SyfosyketilfelleApiSpek.kt
+++ src/test/kotlin/no/nav/syfo/syfosyketilfelle/api/SyfosyketilfelleApiSpek.kt
@@ -5,10 +5,13 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.http.*
 import io.ktor.http.HttpHeaders.Authorization
 import io.ktor.server.testing.*
+import no.nav.syfo.syfosyketilfelle.domain.KOppfolgingstilfelle
 import no.nav.syfo.syfosyketilfelle.domain.KOppfolgingstilfellePerson
 import no.nav.syfo.testhelper.*
 import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_AKTORID
 import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID
+import no.nav.syfo.testhelper.UserConstants.ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR
+import no.nav.syfo.testhelper.UserConstants.VIRKSOMHETSNUMMER_DEFAULT
 import no.nav.syfo.util.bearerHeader
 import org.amshove.kluent.shouldBeEqualTo
 import org.spekframework.spek2.Spek
@@ -36,10 +39,10 @@ class SyfosyketilfelleApiSpek : Spek({
                 externalMockEnvironment = externalMockEnvironment,
             )
 
-            val urlOppfolgingstilfellePersonWithParam =
-                "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_AKTORID.value}"
+            describe("Get OppfolgingstilfellePerson for person from Syfosyketilfelle") {
+                val urlOppfolgingstilfellePerson =
+                    "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_AKTORID.value}"
 
-            describe("Get OppfolgingstilfellePerson from Syfosyketilfelle") {
                 val validToken = generateJWT(
                     externalMockEnvironment.environment.aadAppClient,
                     externalMockEnvironment.wellKnown.issuer,
@@ -50,7 +53,7 @@ class SyfosyketilfelleApiSpek : Spek({
 
                     it("should return OK if request is successful") {
                         with(
-                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonWithParam) {
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePerson) {
                                 addHeader(Authorization, bearerHeader(validToken))
                             }
                         ) {
@@ -76,11 +79,11 @@ class SyfosyketilfelleApiSpek : Spek({
                     }
 
                     it("should return the same error status as Syfosyketilfelle") {
-                        val urlOppfolgingstilfellePersonNoOppfolgingstilfelle =
-                            "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${UserConstants.ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}"
+                        val urlOppfolgingstilfellePersonError =
+                            "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}"
 
                         with(
-                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonNoOppfolgingstilfelle) {
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonError) {
                                 addHeader(Authorization, bearerHeader(validToken))
                             }
                         ) {
@@ -90,7 +93,7 @@ class SyfosyketilfelleApiSpek : Spek({
 
                     it("should return status Unauthorized if no token is supplied") {
                         with(
-                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonWithParam) {}
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePerson) {}
                         ) {
                             response.status() shouldBeEqualTo HttpStatusCode.Unauthorized
                         }
@@ -104,7 +107,165 @@ class SyfosyketilfelleApiSpek : Spek({
                         )
 
                         with(
-                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonWithParam) {
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePerson) {
+                                addHeader(Authorization, bearerHeader(validTokenUnauthorizedAZP))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.Forbidden
+                        }
+                    }
+                }
+            }
+
+            describe("Get OppfolgingstilfellePerson for Person without Arbeidsgiver/Virksomhet from Syfosyketilfelle") {
+                val urlOppfolgingstilfellePersonNoArbeidsgiver =
+                    "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_AKTORID.value}$syfosyketilfelleProxyOppfolgingstilfellePersonUtenArbeidsgiverPath"
+
+                val validToken = generateJWT(
+                    externalMockEnvironment.environment.aadAppClient,
+                    externalMockEnvironment.wellKnown.issuer,
+                    testIsdialogmoteClientId,
+                )
+
+                describe("Happy path") {
+
+                    it("should return OK if request is successful") {
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonNoArbeidsgiver) {
+                                addHeader(Authorization, bearerHeader(validToken))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.OK
+                            val oppfolgingstilfellePersonResponse =
+                                objectMapper.readValue<KOppfolgingstilfellePerson>(response.content!!)
+                            oppfolgingstilfellePersonResponse shouldBeEqualTo externalMockEnvironment.syfosyketilfelleMock.oppfolgingstilfellePersonResponse
+                        }
+                    }
+                }
+                describe("Unhappy paths") {
+                    it("should return status NoContent if Person has no Oppfolgingstilfelle") {
+                        val urlOppfolgingstilfellePersonNoArbeidsgiverNoOppfolgingstilfelle =
+                            "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID.value}$syfosyketilfelleProxyOppfolgingstilfellePersonUtenArbeidsgiverPath"
+
+                        with(
+                            handleRequest(
+                                HttpMethod.Get,
+                                urlOppfolgingstilfellePersonNoArbeidsgiverNoOppfolgingstilfelle
+                            ) {
+                                addHeader(Authorization, bearerHeader(validToken))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.NoContent
+                        }
+                    }
+
+                    it("should return the same error status as Syfosyketilfelle") {
+                        val urlOppfolgingstilfellePersonNoArbeidsgiverError =
+                            "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}$syfosyketilfelleProxyOppfolgingstilfellePersonUtenArbeidsgiverPath"
+
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonNoArbeidsgiverError) {
+                                addHeader(Authorization, bearerHeader(validToken))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.BadRequest
+                        }
+                    }
+
+                    it("should return status Unauthorized if no token is supplied") {
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonNoArbeidsgiver) {}
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.Unauthorized
+                        }
+                    }
+
+                    it("should return status Forbidden if unauthorized AZP is supplied") {
+                        val validTokenUnauthorizedAZP = generateJWT(
+                            externalMockEnvironment.environment.aadAppClient,
+                            externalMockEnvironment.wellKnown.issuer,
+                            testIsnarmestelederClientId,
+                        )
+
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonNoArbeidsgiver) {
+                                addHeader(Authorization, bearerHeader(validTokenUnauthorizedAZP))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.Forbidden
+                        }
+                    }
+                }
+            }
+
+            describe("Get Oppfolgingstilfelle for Person and Virksomhet from Syfosyketilfelle") {
+                val urlOppfolgingstilfellePersonVirksomhet =
+                    "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_AKTORID.value}/${VIRKSOMHETSNUMMER_DEFAULT.value}"
+
+                val validToken = generateJWT(
+                    externalMockEnvironment.environment.aadAppClient,
+                    externalMockEnvironment.wellKnown.issuer,
+                    testIsdialogmoteClientId,
+                )
+
+                describe("Happy path") {
+
+                    it("should return OK if request is successful") {
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonVirksomhet) {
+                                addHeader(Authorization, bearerHeader(validToken))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.OK
+                            val oppfolgingstilfellePersonVirksomhetResponse =
+                                objectMapper.readValue<KOppfolgingstilfelle>(response.content!!)
+                            oppfolgingstilfellePersonVirksomhetResponse shouldBeEqualTo externalMockEnvironment.syfosyketilfelleMock.oppfolgingstilfelleResponse
+                        }
+                    }
+                }
+                describe("Unhappy paths") {
+                    it("should return status NoContent if Person has no Oppfolgingstilfelle") {
+                        val urlOppfolgingstilfellePersonVirksomhetNoOppfolgingstilfelle =
+                            "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID.value}/${VIRKSOMHETSNUMMER_DEFAULT.value}"
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonVirksomhetNoOppfolgingstilfelle) {
+                                addHeader(Authorization, bearerHeader(validToken))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.NoContent
+                        }
+                    }
+
+                    it("should return the same error status as Syfosyketilfelle") {
+                        val urlOppfolgingstilfellePersonVirksomhetError =
+                            "$syfosyktilfelleProxyBasePath$syfosyketilfelleProxyOppfolgingstilfellePersonPath/${ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR.value}"
+
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonVirksomhetError) {
+                                addHeader(Authorization, bearerHeader(validToken))
+                            }
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.BadRequest
+                        }
+                    }
+
+                    it("should return status Unauthorized if no token is supplied") {
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonVirksomhet) {}
+                        ) {
+                            response.status() shouldBeEqualTo HttpStatusCode.Unauthorized
+                        }
+                    }
+
+                    it("should return status Forbidden if unauthorized AZP is supplied") {
+                        val validTokenUnauthorizedAZP = generateJWT(
+                            externalMockEnvironment.environment.aadAppClient,
+                            externalMockEnvironment.wellKnown.issuer,
+                            testIsnarmestelederClientId,
+                        )
+
+                        with(
+                            handleRequest(HttpMethod.Get, urlOppfolgingstilfellePersonVirksomhet) {
                                 addHeader(Authorization, bearerHeader(validTokenUnauthorizedAZP))
                             }
                         ) {
--- src/test/kotlin/no/nav/syfo/testhelper/UserConstants.kt
+++ src/test/kotlin/no/nav/syfo/testhelper/UserConstants.kt
@@ -1,12 +1,15 @@
 package no.nav.syfo.testhelper
 
 import no.nav.syfo.domain.AktorId
+import no.nav.syfo.domain.Virksomhetsnummer
 
 object UserConstants {
     val ARBEIDSTAKER_PERSONIDENT = "12345678912"
     val ARBEIDSTAKER_AKTORID = AktorId("1234567891201")
     val ARBEIDSTAKER_NO_OPPFOLGINGSTILFELLE_AKTORID = AktorId(ARBEIDSTAKER_AKTORID.value.replace("1", "2"))
     val ARBEIDSTAKER_OPPFOLGINGSTILFELLE_ERROR = AktorId(ARBEIDSTAKER_AKTORID.value.replace("1", "3"))
+
+    val VIRKSOMHETSNUMMER_DEFAULT = Virksomhetsnummer("912345678")
 
     const val ENHET_NR = "0123"
 
